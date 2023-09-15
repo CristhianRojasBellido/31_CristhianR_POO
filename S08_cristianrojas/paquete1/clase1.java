a50418fb98c47373f02c0e13ae96888c98542e1b
Cristhian Manuel Rojas Bellido <131499371+CristhianRojasBellido@users.noreply.github.com>
2023-09-15 09:20:42 -0500

    Add files via upload

---
+++ S08_cristianrojas/paquete1/clase1.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package S08_cristianrojas.paquete1;
+
+/**
+ *
+ * @author CristhianRojas
+ */
+public class clase1 {
+    public int a;
+    private int b;
+}
---
+++ S08_cristianrojas/paquete2/clase3/clase3.java
@@ -0,0 +1,11 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package S08_cristianrojas.paquete2.clase3;
+
+public class clase3 {
+    public int a;
+    private int b;
+}
